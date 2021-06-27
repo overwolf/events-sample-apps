e80e95d3581a18e8af86cc56f38f9293c6997ecc
eran <eran.sharvit@overwolf.com>
2021-06-27 17:19:25 +0300

    Update main.js

--- underlords-events-sample-app/main.js
+++ underlords-events-sample-app/main.js
@@ -1,4 +1,4 @@
-// this a subset of the features that Dota Underlordsevents provides - however,
+// this a subset of the features that Dota Underlords events provides - however,
 // when writing an app that consumes events - it is best if you request
 // only those features that you want to handle.
 //
